6915f00a44aeee637ae78136d29aef2effa3027b
Szalovszky Dávid <david@szalovszky.com>
2024-01-22 00:43:49 +0100

    ref: construct new response to return

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,22 @@ const handler: ExportedHandler<Env> = {
 
     const url = new URL(request.url);
     if (url.pathname !== '/') return new Response('Not found', { status: 404 });
-		
-		const urlParam = url.searchParams.get('url');
+
+    const urlParam = url.searchParams.get('url');
     if (!urlParam || !url.searchParams.has('url')) return new Response('Missing URL parameter', { status: 400 });
 
-		const requestUrl = new URL(urlParam);
-		if (!env.CONFIG.WHITELIST.includes(requestUrl.hostname)) return new Response('Domain not whitelisted', { status: 403 });
+    const requestUrl = new URL(urlParam);
+    if (!env.CONFIG.WHITELIST.includes(requestUrl.hostname))
+      return new Response('Domain not whitelisted', { status: 403 });
 
     const response = await fetch(url.searchParams.get('url') as string);
-    if (!response.ok) return new Response('Internal error', { status: 500 });
-    if (!response.headers.get('content-type')?.startsWith('image/')) return new Response('Not an image', { status: 400 });
+    if (!response.ok || !response.body) return new Response('Internal error', { status: 500 });
 
-    return response;
+    const contentType = response.headers.get('content-type');
+    if (!contentType?.startsWith('image/')) return new Response('Not an image', { status: 400 });
+
+    const blob = await response.blob();
+    return new Response(await blob.arrayBuffer(), { status: 200, headers: { 'content-type': contentType } });
   },
 };
 
