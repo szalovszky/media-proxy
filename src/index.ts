7ed5a1505dfbf501d21ea393d20e67fbdd040825
Szalovszky Dávid <david@szalovszky.com>
2024-01-21 23:45:31 +0100

    ref: use ExportedHandler type

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-export default {
-	async fetch(request: Request, ctx: ExecutionContext): Promise<Response> {
-		if (request.method !== 'GET') return new Response('Method not allowed', { status: 405 });
+const handler: ExportedHandler = {
+  async fetch(request) {
+    if (request.method !== 'GET') return new Response('Method not allowed', { status: 405 });
 
-		const url = new URL(request.url);
-		if (url.pathname !== '/') return new Response('Not found', { status: 404 });
-		if (!url.searchParams.has('url')) return new Response('Missing URL parameter', { status: 400 });
+    const url = new URL(request.url);
+    if (url.pathname !== '/') return new Response('Not found', { status: 404 });
+    if (!url.searchParams.has('url')) return new Response('Missing URL parameter', { status: 400 });
 
-		const result = await fetch(url.searchParams.get('url') as string);
-		if (!result.ok) return new Response('Internal error', { status: 500 });
-		if (!result.headers.get('content-type')?.startsWith('image/')) return new Response('Not an image', { status: 400 });
+    const result = await fetch(url.searchParams.get('url') as string);
+    if (!result.ok) return new Response('Internal error', { status: 500 });
+    if (!result.headers.get('content-type')?.startsWith('image/')) return new Response('Not an image', { status: 400 });
 
-		return result;
-	},
+    return result;
+  },
 };
+
+export default handler;
