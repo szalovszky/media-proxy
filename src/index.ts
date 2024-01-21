46a57db2469ce00a2f4d09a005d2d53cf5a6b67d
Szalovszky Dávid <david@szalovszky.com>
2024-01-22 00:22:52 +0100

    feat: domain whitelist

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-const handler: ExportedHandler = {
-  async fetch(request) {
+export interface Env {
+  CONFIG: { WHITELIST: string[] };
+}
+
+const handler: ExportedHandler<Env> = {
+  async fetch(request, env) {
     if (request.method !== 'GET') return new Response('Method not allowed', { status: 405 });
 
     const url = new URL(request.url);
     if (url.pathname !== '/') return new Response('Not found', { status: 404 });
-    if (!url.searchParams.has('url')) return new Response('Missing URL parameter', { status: 400 });
+		
+		const urlParam = url.searchParams.get('url');
+    if (!urlParam || !url.searchParams.has('url')) return new Response('Missing URL parameter', { status: 400 });
 
-    const result = await fetch(url.searchParams.get('url') as string);
-    if (!result.ok) return new Response('Internal error', { status: 500 });
-    if (!result.headers.get('content-type')?.startsWith('image/')) return new Response('Not an image', { status: 400 });
+		const requestUrl = new URL(urlParam);
+		if (!env.CONFIG.WHITELIST.includes(requestUrl.hostname)) return new Response('Domain not whitelisted', { status: 403 });
 
-    return result;
+    const response = await fetch(url.searchParams.get('url') as string);
+    if (!response.ok) return new Response('Internal error', { status: 500 });
+    if (!response.headers.get('content-type')?.startsWith('image/')) return new Response('Not an image', { status: 400 });
+
+    return response;
   },
 };
 
