4a8d928f2c5903a1cde77f07549b75f66fd684e7
Szalovszky Dávid <david@szalovszky.com>
2024-01-21 23:27:39 +0100

    feat: ignore non-image resources

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 export default {
 	async fetch(request: Request, ctx: ExecutionContext): Promise<Response> {
 		if (request.method !== 'GET') return new Response('Method not allowed', { status: 405 });
+
 		const url = new URL(request.url);
 		if (url.pathname !== '/') return new Response('Not found', { status: 404 });
 		if (!url.searchParams.has('url')) return new Response('Missing URL parameter', { status: 400 });
-		return await fetch(url.searchParams.get('url') as string);
+
+		const result = await fetch(url.searchParams.get('url') as string);
+		if (!result.ok) return new Response('Internal error', { status: 500 });
+		if (!result.headers.get('content-type')?.startsWith('image/')) return new Response('Not an image', { status: 400 });
+
+		return result;
 	},
 };
